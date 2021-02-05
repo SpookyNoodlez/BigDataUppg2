06452a3e866b1f58f4874256071089a0ba9f7730
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2021-02-05 09:38:39 +0200

    uppg2 klar

--- uppg1.py
+++ uppg1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-topGames = {"Top selling games": ["Minecraft","Grand Theft Auto V", "Tetris", "Wii Sports"],
-        "Sales": [200000000, 135000000, 100000000, 82900000]
+topGames = {'Top selling games': ['Minecraft','Grand Theft Auto V', 'Tetris', 'Wii Sports', 'PlayerUnknowns Battlegrounds'],
+        'Sales': [200000000, 135000000, 100000000, 82900000, 70000000]
         }
 
 df = pd.DataFrame(topGames, columns = ["Top selling games", "Sales"])
---
+++ uppg2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+topGames = {'Minecraft': 200000000,
+                   'Grand Theft Auto V': 135000000,
+                   'Tetris': 100000000,
+                   'Wii Sports': 82900000,
+                   'PlayerUnknowns Battlegrounds': 70000000}
+
+
+
+
+print(pd.Series(topGames))
