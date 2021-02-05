5fbb43c1cb48db2968fdca36df7e58dee49283ed
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2021-02-05 08:45:33 +0200

    klar

---
+++ uppg1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+topGames = {"Top selling games": ["Minecraft","Grand Theft Auto V", "Tetris", "Wii Sports"],
+        "Sales": [200000000, 135000000, 100000000, 82900000]
+        }
+
+df = pd.DataFrame(topGames, columns = ["Top selling games", "Sales"])
+
+print(df)
+
+print("\n\n")
+
+#Printar raden med index 1
+print(df.iloc[[1]])
+
+print("\n\n")
+
+#Printar raden med index 3
+print(df.iloc[[3]])
